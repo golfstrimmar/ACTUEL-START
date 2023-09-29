06100cc2342b4ce0b50f5dd4e613ba090a90de23
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-09-29 13:21:35 +0200

    lll

--- src/modules/form/form.js
+++ src/modules/form/form.js
@@ -1,7 +1,63 @@
 "use ctrict";
 
 export const Form = () => {
-  alert("");
+  let FirstName = document.querySelector("#firstname");
+  let email = document.querySelector("#email");
+  let tel = document.querySelector("#tel");
+  let BookForm = document.querySelector("#send-form");
+
+  BookForm.addEventListener("submit", (e) => {
+    if (!FirstName.value) {
+      FirstName.closest(".input-field").classList.add("_check_invalid");
+      setTimeout(() => {
+        window.scrollTo({
+          top: BookForm.offsetTop - 100,
+          left: 0,
+          behavior: "smooth",
+        });
+      }, 200);
+
+      e.preventDefault();
+    } else if (
+      FirstName.closest(".input-field").classList.contains("_check_invalid")
+    ) {
+      FirstName.closest(".input-field").classList.remove("_check_invalid");
+    }
+
+    if (!email.value) {
+      email.closest(".input-field").classList.add("_check_invalid");
+      setTimeout(() => {
+        window.scrollTo({
+          top: BookForm.offsetTop - 100,
+          left: 0,
+          behavior: "smooth",
+        });
+      }, 200);
+
+      e.preventDefault();
+    } else if (
+      email.closest(".input-field").classList.contains("_check_invalid")
+    ) {
+      email.closest(".input-field").classList.remove("_check_invalid");
+    }
+
+    if (!tel.value) {
+      tel.closest(".input-field").classList.add("_check_invalid");
+      setTimeout(() => {
+        window.scrollTo({
+          top: BookForm.offsetTop - 100,
+          left: 0,
+          behavior: "smooth",
+        });
+      }, 200);
+
+      e.preventDefault();
+    } else if (
+      tel.closest(".input-field").classList.contains("_check_invalid")
+    ) {
+      tel.closest(".input-field").classList.remove("_check_invalid");
+    }
+  });
 };
 
 // export class  {
