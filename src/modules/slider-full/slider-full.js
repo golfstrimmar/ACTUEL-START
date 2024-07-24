008faad142f95910055b368b72a69c5576f3500b
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-07-24 11:05:17 +0200

    hhh

--- src/modules/slider-full/slider-full.js
+++ src/modules/slider-full/slider-full.js
@@ -2,35 +2,31 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 
-export const SwiperFull = () =>{
-    const swiperFull = new Swiper("#slider-full", {
-      slidesPerView: 2,
-      loop: "true",
-      speed: 500,
-      spaceBetween: 48,
-      centeredSlides: true,
-      grabCursor: true,
-      // effect: "coverflow",
-      modules: [Navigation, Pagination],
-      // coverflowEffect: {
-      //   rotate: 3,
-      //   stretch: 0,
-      //   depth: 120,
-      //   // modifier: 1,
-      //   // slideShadows: true,
-      // },
-      // watchSlidesProgress: true,
-      pagination: {
-        el: ".swiper-pagination-full",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".js-full-next",
-        prevEl: ".js-full-prev",
-      },
-    });
-  }
-
-
-
-
+export const SwiperFull = () => {
+  const swiperFull = new Swiper("#slider-full", {
+    slidesPerView: 2,
+    loop: "true",
+    speed: 500,
+    spaceBetween: 65,
+    // centeredSlides: true,
+    grabCursor: true,
+    // effect: "coverflow",
+    modules: [Navigation, Pagination],
+    // coverflowEffect: {
+    //   rotate: 3,
+    //   stretch: 0,
+    //   depth: 120,
+    //   // modifier: 1,
+    //   // slideShadows: true,
+    // },
+    // watchSlidesProgress: true,
+    pagination: {
+      el: ".swiper-pagination-full",
+      clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".js-full-next",
+    //   prevEl: ".js-full-prev",
+    // },
+  });
+};
