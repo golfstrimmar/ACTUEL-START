0c050f44d1e2da257a89061b0c5693ff879dbd4a
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-09-14 21:44:20 +0200

    ааааа

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,11 @@
 import "./import/modules";
 import "./import/components";
-<<<<<<< HEAD
 
 // import { createApp } from "vue";
-=======
-import { createApp } from "vue";
-import { createPinia } from "pinia";
->>>>>>> 55b37383e5dce04d24da4a0cc4ff976f6382ee64
 // import VueDatePicker from "@vuepic/vue-datepicker";
 
 // import Rooms from "./../familia/rooms/Rooms.vue";
-import Stell from "./../modules/stellen-vue/Stell.vue";
+// import Stell from "./../modules/stellen-vue/Stell.vue";
 // import CheckIn from "../blocks/components/vue-components/CheckIn.vue";
 // import CheckOut from "../blocks/components/vue-components/CheckOut.vue";
 
@@ -18,6 +13,6 @@ import Stell from "./../modules/stellen-vue/Stell.vue";
 // createApp(CheckOut)
 // .component("VueDatePicker", VueDatePicker)
 // .mount("#check-out");
-[...document.querySelectorAll(".stell")].forEach((cell) => {
-  createApp(Stell).use(createPinia()).mount(cell);
-});
+// [...document.querySelectorAll(".stell")].forEach((cell) => {
+//   createApp(Stell).use(createPinia()).mount(cell);
+// });
