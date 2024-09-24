310c83ee2af8b19625d674f4fb6c24ea858aa8ef
golfmar <yushinbox@gmail.com>
2024-09-24 21:59:21 +0200

    www

--- src/js/import/components.js
+++
@@ -1,8 +0,0 @@
-import { Button } from "%components%/btn-wave/btn-wave";
-
-document.addEventListener("DOMContentLoaded", function () {
-  //   // ==========Button=============
-  [...document.querySelectorAll(".but-wave")].forEach((cell) => {
-    cell.addEventListener("click", Button);
-  });
-});
